6322fd0a369799a7a356b0342220f629a1a2a70d
LAPTOP-G5629H2U\Jae Hyunhyo <jaehh1987@gmail.com>
2019-01-02 18:24:06 -0800

    finish solution class
    add method to create solution array and return it to
    where it is called.

--- src/Solution.java
+++ src/Solution.java
@@ -70,9 +70,81 @@ public class Solution {
      * method to fill rest of boxes
      */
     public void fillRemaining() {
-        
+        // loop through all cells in the solution array
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                // if cell is empty, fill it with possible number
+                if (solution[row][col] == 0) {
+                    fillCell(row, col);
+                }
+            }
+        }
     }
     
+    /*
+     * method to fill one single cell of the solution array
+     */
+    public void fillCell(int row, int col) {
+        for (int candidate = 1; candidate < size; candidate++) {
+            if (check(row, col, candidate)) {
+                solution[row][col] = candidate;
+                return;
+            }
+        }
+    }
+    
+    /*
+     * method to check availability of certain number in specific position
+     * if candidate can fill in solution[row][col] return true, else return false.
+     */
+    public boolean check(int row, int col, int candidate) {
+        boolean result = (
+                checkBlock(row, col, candidate)
+                && checkRow(row, candidate)
+                && checkCol(col, candidate)
+                );
+        return result;
+    }
+    
+    /*
+     * method to check availability of certain number in the block
+     */
+    public boolean checkBlock(int row, int col, int candidate) {
+        int blockStartRow = 3 * (row / 3);
+        int blockStartCol = 3 * (col / 3);
+        for (int i = blockStartRow; i < blockStartRow + blockSize; i++) {
+            for (int j = blockStartCol; j < blockStartCol + blockSize; j++) {
+                if (solution[i][j] == candidate) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    /* 
+     * method to check availability of certain number in the row
+     */
+    public boolean checkRow(int row, int candidate) {
+        for (int i = 0; i < size; i++) {
+            if (solution[row][i] == candidate) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /*
+     * method to check availability of certain number in the column
+     */
+    public boolean checkCol(int col, int candidate) {
+        for (int i = 0; i < size; i++) {
+            if (solution[i][col] == candidate) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 

2b6d1035a155f81a5257971e89769748f7fde7ae
LAPTOP-G5629H2U\Jae Hyunhyo <jaehh1987@gmail.com>
2019-01-03 22:37:48 -0800

    build GUI
    minor changes in Main.java and Displayer.java

--- src/Displayer.java
+++ src/Displayer.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -17,11 +18,13 @@ public class Displayer {
         this.game = game;
         this.grid = grid;
         
-        textField = new TextField[9][9];
-        for(int i = 0; i < 9; i++) {
-        	for(int j = 0; j < 9; j++) {
+        textField = new TextField[game.size][game.size];
+        for(int i = 0; i < game.size; i++) {
+        	for(int j = 0; j < game.size; j++) {
         		textField[i][j] = new TextField(Integer.toString(game.solution[i][j]));
-            	textField[i][j].setMinWidth(10);
+            	textField[i][j].setPrefWidth(30);
+            	textField[i][j].setPrefHeight(30);
+            	textField[i][j].setAlignment(Pos.CENTER);
         	}
         }
     }
@@ -75,8 +78,8 @@ public class Displayer {
     }
     
     public void gameBoard() {
-        for(int i = 0; i < 9; i++) {
-        	for(int j = 0; j < 9; j++) {
+        for(int i = 0; i < game.size; i++) {
+        	for(int j = 0; j < game.size; j++) {
         		grid.add(textField[j][i], i, j);
         	}
         }
--- src/Main.java
+++ src/Main.java
@@ -27,17 +27,17 @@ public class Main extends Application{
         // integer represents Sudoku puzzle size
         int size = 9;
         
-        // instantiate game and displayer object
-        // displayer object takes game object as an argument
-        game = new Game(size);
-        displayer = new Displayer(game, grid);
-        
         // check the integer size is perfect square or not
         Double sqrtSize = Math.sqrt(size);
         if (sqrtSize - Math.floor(sqrtSize) != 0) {
             System.out.println("Size should be perfect square number!");
             return;
         }
+        
+        // instantiate game and displayer object
+        // displayer object takes game object as an argument
+        game = new Game(size);
+        displayer = new Displayer(game, grid);
         
         displayer.play();
     }
